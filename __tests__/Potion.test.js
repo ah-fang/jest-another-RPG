3a537f88b7e9a0880521ae0745e32e7660f83071
ah-fang <94493319+ah-fang@users.noreply.github.com>
2022-05-16 17:48:40 -0700

    potion test

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,10 @@
+const Potion = require("../lib/Potion.js");
+
+test('Creates a random potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+    
+});

1dcaaf773cd00c2bc151396aa67ef2206954b7d2
ah-fang <94493319+ah-fang@users.noreply.github.com>
2022-05-19 17:42:35 -0700

    game object

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -8,3 +8,11 @@ test('Creates a random potion object', () => {
     expect(potion.value).toEqual(expect.any(Number));
     
 });
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+  
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
